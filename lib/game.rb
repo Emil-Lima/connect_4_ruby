dc467b08af872e2c71ece08d14cf9af866209eb9
J-Rozas <jrozaslima@gmail.com>
2021-01-14 19:32:30 +0000

    Small refactoring to these files.

--- lib/game.rb
+++ lib/game.rb
@@ -7,7 +7,7 @@ class Game
   attr_accessor :player1, :player2
 
   def initialize
-    @player1 = Player.new('Dan')
-    @player2 = Player.new('Laura')
+    @player1 = Player.new('player1')
+    @player2 = Player.new('player2')
   end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
+require './lib/multi_function'
+
 # class that creates the players of the game
 class Player
-  attr_accessor :name
+  attr_accessor :name, :moves
+
+  include MultiFunction
 
   def initialize(name)
     @name = name
+    @moves = [1, 1, 1, 1]
   end
 end
