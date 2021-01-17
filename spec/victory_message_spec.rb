8c05012b81b438e8b405a3662261e5f722aacca7
J-Rozas <jrozaslima@gmail.com>
2021-01-17 00:13:28 +0000

    Updated test.

--- spec/victory_message_spec.rb
+++ spec/victory_message_spec.rb
@@ -1,15 +1,22 @@
 # frozen_string_literal: true
 
-# Comment out all the require in game.rb to make the tests work
+# To pass the text do the 2 following things:
+# On file game.rb, method #play, add <return "player1 wins">.
+# On file multi_function.rb, method #player_turn(player), <col_player = "1">.
 
 require './lib/player'
 require './lib/game'
+require "./lib/grid"
+require "./lib/multi_function"
+require "./lib/main"
 
 describe Game do
   describe '#victory_message' do
     it 'returns a victory message with name' do
       new_game = Game.new
-      expect(new_game.player1.victory).to eql('player1 wins!')
+      new_game.grid.background_grid = [['|x x x x x x x|'], ['|x x x x x x x|'], ['|x x x x x x x|'], ['|x x x x x x x|'],
+      ['|x x x x x x x|'], ['|x x x x x x x|']]
+      expect(new_game.play).to eql('player1 wins!')
     end
   end
 end
