2c28e13394dbb1a52320da7721ecd1c82099497a
J-Rozas <jrozaslima@gmail.com>
2021-01-17 22:13:48 +0000

    Updated the test.

--- spec/victory_message_spec.rb
+++ spec/victory_message_spec.rb
@@ -6,16 +6,16 @@
 
 require './lib/player'
 require './lib/game'
-require "./lib/grid"
-require "./lib/multi_function"
-require "./lib/main"
+require './lib/grid'
+require './lib/multi_function'
+require './lib/main'
 
 describe Game do
   describe '#victory_message' do
     it 'returns a victory message with name' do
       new_game = Game.new
       new_game.grid.background_grid = [['|x x x x x x x|'], ['|x x x x x x x|'], ['|x x x x x x x|'], ['|x x x x x x x|'],
-      ['|x x x x x x x|'], ['|x x x x x x x|']]
+                                       ['|x x x x x x x|'], ['|x x x x x x x|']]
       expect(new_game.play).to eql('player1 wins!')
     end
   end
