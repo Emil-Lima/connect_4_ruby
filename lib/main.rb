89e9c5bc64bb57b66a07845b3005aae777afd441
J-Rozas <jrozaslima@gmail.com>
2021-01-18 21:44:10 +0000

    Created a new class that runs the whole game and included a restart method in such class.

--- lib/main.rb
+++ lib/main.rb
@@ -2,6 +2,23 @@
 
 require './lib/game'
 
-game = Game.new
-game.first_grid
-game.play
+# Class that runs the full game
+class Main
+  def self_initialize
+    game = Game.new
+    game.first_grid
+    game.play
+    self_replay
+  end
+
+  def self_replay
+    puts 'Do you want to play again?(y/n)'
+    answer = gets.chomp!
+    return unless answer == 'y'
+
+    puts "The new game is starting now:\n "
+    Main.new.self_initialize
+  end
+end
+
+Main.new.self_initialize
