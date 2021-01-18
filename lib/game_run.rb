2220f5618934b60c22af943d8987e547425c53b1
J-Rozas <jrozaslima@gmail.com>
2021-01-18 22:00:07 +0000

    Added a new module that controls the flow of the game and updated the main.rb file to reflect this change.

---
+++ lib/game_run.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require './lib/game'
+
+# Module with a method that actually runs the game and a method to replay the game
+module GameRun
+  def full_game
+    game = Game.new
+    game.first_grid
+    game.play
+    replay
+  end
+
+  def replay
+    print 'Do you want to play again?(type y if yes): '
+    answer = gets.chomp!
+    if answer == 'y'
+      puts "The new game is starting now:\n "
+      full_game
+    else
+      puts 'Thanks for playing!'
+    end
+  end
+end
--- lib/main.rb
+++ lib/main.rb
@@ -1,24 +1,6 @@
 # frozen_string_literal: true
 
-require './lib/game'
+require './lib/game_run'
 
-# Class that runs the full game
-class Main
-  def self_initialize
-    game = Game.new
-    game.first_grid
-    game.play
-    self_replay
-  end
-
-  def self_replay
-    puts 'Do you want to play again?(y/n)'
-    answer = gets.chomp!
-    return unless answer == 'y'
-
-    puts "The new game is starting now:\n "
-    Main.new.self_initialize
-  end
-end
-
-Main.new.self_initialize
+include GameRun
+full_game
